ee0ba04d794fe0b00d38e3fad2781c31bd3637c6
TheoR <theo.ritouni@modulo-pi.com>
2022-05-10 17:37:03 +0200

    inluence setting on unit , optimize some part

--- Assets/Scripts/AI/InfluenceMap.cs
+++ Assets/Scripts/AI/InfluenceMap.cs
@@ -16,19 +16,21 @@ public class InfluenceMap : MonoBehaviour
     private Texture2D limitMap;
     [SerializeField]
     private Material mat;
+
     private Vector2 sizeField;
     private Terrain terrain;
+
+    [Header("Influence Settings")]
+    [SerializeField]
+    private float powerInfluence = 1.0f;
+    [SerializeField]
+    private float radiusInfluence = 5.0f;
 
     [Space]
     [SerializeField]
     private float timerUpdate = 1.0f;
     private float timerSave = 0.0f;
 
-    [Header("Debug")]
-    [SerializeField]
-    private bool isDebug = false;
-    [SerializeField]
-    private float divTerrain = 30.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +40,8 @@ public class InfluenceMap : MonoBehaviour
         terrain = GetComponent<Terrain>();
         sizeField.x = terrain.terrainData.size.x;
         sizeField.y = terrain.terrainData.size.z;
+        
+        GenerateLimitInfluenceMap();
         GenerateInfluenceMap();
     }
 
@@ -52,91 +56,8 @@ public class InfluenceMap : MonoBehaviour
         }
         
     }
-
-    private void OnDrawGizmos()
+    void GenerateLimitInfluenceMap()
     {
-        if (!isDebug)
-            return;
-
-        float height = 10.0f;
-
-        for (float i = 0; i < sizeField.x; i += sizeField.x / divTerrain)
-        {
-            Gizmos.color = Color.white;
-            Gizmos.DrawLine(new Vector3(i, height, 0), new Vector3(i, height, sizeField.x));
-
-            for (float j = 0; j < sizeField.y; j += sizeField.y / divTerrain)
-            {
-                float ratioMapTexture = limitMap.width / sizeField.x;
-
-                Color posPixel = limitMap.GetPixel((int)(i * ratioMapTexture), (int)(j * ratioMapTexture));
-
-                float minColor = 0.1f;
-
-                if (posPixel.r > minColor || posPixel.g > minColor || posPixel.b > minColor)
-                    Gizmos.color = Color.yellow;
-                else if (CheckBuilding(new Vector2(i, j), 5))
-                    Gizmos.color = Color.green;
-                else
-                    Gizmos.color = Color.white;
-
-                Gizmos.DrawSphere(new Vector3(i, height, j), 0.5f);
-                
-                if (i == 0)
-                {
-                    Gizmos.color = Color.white;
-                    Gizmos.DrawLine(new Vector3(0, height, j), new Vector3(sizeField.y, height, j));
-                }
-            }
-        }   
-    }
-
-    bool CheckBuilding(Vector2 pos, float maxDistance)
-    {
-
-        TargetBuilding[] savebuilding = GameServices.GetTargetBuildings();
-
-        for (int i = 0; i < savebuilding.Length; i++)
-        {
-            Vector2 flatPos = new Vector2(savebuilding[i].gameObject.transform.position.x, savebuilding[i].gameObject.transform.position.z);
-            if ((flatPos - pos).magnitude < maxDistance)
-                return true;
-        }
-
-        return false;
-    }
-
-    bool CheckUnit(Vector2 pos , ETeam team , float maxdDistance)
-    {
-        List<Unit> units = GameServices.GetControllerByTeam(team).GetAllUnits();
-
-        for(int i = 0 ; i < units.Count ; i++)
-        {
-            Vector2 flatPos = new Vector2(units[i].gameObject.transform.position.x, units[i].gameObject.transform.position.z);
-            if ((flatPos - pos).magnitude < maxdDistance)
-                return true;
-        }
-
-        return false;
-    }
-
-    bool CheckFactory(Vector2 pos, ETeam team, float maxdDistance)
-    {
-        List<Factory> factorys = GameServices.GetControllerByTeam(team).GetAllFactorys();
-
-        for (int i = 0; i < factorys.Count; i++)
-        {
-            Vector2 flatPos = new Vector2(factorys[i].gameObject.transform.position.x, factorys[i].gameObject.transform.position.z);
-            if ((flatPos - pos).magnitude < maxdDistance)
-                return true;
-        }
-
-        return false;
-    }
-
-    void GenerateInfluenceMap()
-    {
-
         mat.mainTexture = influenceTex;
 
         for (int i = 0; i < influenceTex.width; i++)
@@ -144,41 +65,115 @@ public class InfluenceMap : MonoBehaviour
             for (int j = 0; j < influenceTex.height; j++)
             {
                 float ratioMapTexture = limitMap.width / influenceTex.width;
-                float ratioTexMap = influenceTex.width / sizeField.x;
                 float minColor = 0.1f;
 
                 Color posPixel = limitMap.GetPixel((int)(i * ratioMapTexture), (int)(j * ratioMapTexture));
 
-                influenceTex.SetPixel(i, j, Color.white);
+                influenceTex.SetPixel(i, j, Color.black);
 
                 // Area empty you can't do nothing on it 
-                if (posPixel.r > minColor || posPixel.g > minColor || posPixel.b > minColor)
-                    influenceTex.SetPixel(i,j,Color.black);
-
-                float ratioI = i / ratioTexMap;
-                float ratioJ = j / ratioTexMap;
-
-                // Check if Building
-                if (CheckBuilding(new Vector2(ratioI, ratioJ), 3))
-                    influenceTex.SetPixel(i,j,Color.green);
-               
-                // Units on field 
-                if (CheckUnit(new Vector2(ratioI, ratioJ),ETeam.Blue,2))
-                    influenceTex.SetPixel(i, j, Color.blue);
-
-                if (CheckUnit(new Vector2(ratioI, ratioJ), ETeam.Red, 2))
-                    influenceTex.SetPixel(i, j, Color.red);
-
-                // Factory Make
-                if (CheckFactory(new Vector2(ratioI, ratioJ), ETeam.Blue, 5))
-                    influenceTex.SetPixel(i, j, Color.blue);
-
-                if (CheckFactory(new Vector2(ratioI, ratioJ), ETeam.Red, 5))
-                    influenceTex.SetPixel(i, j, Color.red);
-
+                if (posPixel.r > minColor)
+                    influenceTex.SetPixel(i, j, Color.white);
             }
         }
 
         influenceTex.Apply();
+
+    }
+
+    void GenerateInfluenceMap()
+    {
+        mat.mainTexture = influenceTex;
+
+        for (int i = 0; i < influenceTex.width; i++)
+            for (int j = 0; j < influenceTex.height; j++)
+            {
+                if (influenceTex.GetPixel(i, j) == Color.white)
+                    continue;
+
+                influenceTex.SetPixel(i, j, Color.black);
+            }
+
+        float ratioTexMap = influenceTex.width / sizeField.x;
+
+        CheckUnit(ETeam.Blue, ratioTexMap);
+        CheckUnit(ETeam.Red, ratioTexMap);
+
+        CheckFactory(ETeam.Blue, ratioTexMap);
+        CheckFactory(ETeam.Red, ratioTexMap);
+
+        CheckBuilding(ratioTexMap);
+
+        influenceTex.Apply();
+    }
+
+    void CheckBuilding(float ratio)
+    {
+        TargetBuilding[] savebuilding = GameServices.GetTargetBuildings();
+
+        for (int i = 0; i < savebuilding.Length; i++)
+        {
+            Vector2 flatPos = new Vector2(savebuilding[i].gameObject.transform.position.x * ratio, savebuilding[i].gameObject.transform.position.z * ratio);
+            influenceTex.SetPixel((int)flatPos.x, (int)flatPos.y, Color.green);
+        }
+    }
+
+    void CheckFactory(ETeam team, float ratio)
+    {
+        List<Factory> factorys = GameServices.GetControllerByTeam(team).GetAllFactorys();
+
+        Color color = team == ETeam.Blue ? Color.blue : Color.red;
+
+        for (int i = 0; i < factorys.Count; i++)
+        {
+            Vector2 flatPos = new Vector2(factorys[i].gameObject.transform.position.x * ratio, factorys[i].gameObject.transform.position.z * ratio);
+            influenceTex.SetPixel((int)flatPos.x, (int)flatPos.y, color);
+        }
+    }
+
+    void CheckUnit(ETeam team, float ratio)
+    {
+        List<Unit> units = GameServices.GetControllerByTeam(team).GetAllUnits();
+
+        Color color = team == ETeam.Blue ? Color.blue : Color.red;
+
+        for (int i = 0; i < units.Count; i++)
+        {
+            Vector2 flatPos = new Vector2(units[i].gameObject.transform.position.x * ratio, units[i].gameObject.transform.position.z * ratio);
+            influenceTex.SetPixel((int)flatPos.x, (int)flatPos.y, color);
+
+            SetPixelAroundPositionTexture((int)flatPos.x, (int)flatPos.y, radiusInfluence, color);
+        }
+    }
+
+    void SetPixelAroundPositionTexture(int posX, int posY, float radius, Color color)
+    {
+        Vector2Int centerPixel = new Vector2Int(posX, posY);
+
+        for (int i = posX - (int)radius; i < posX + (int)radius; i++)
+        {
+            for (int j = posY - (int)radius; j < posY + (int)radius; j++)
+            {
+                Color currentColorPixel = influenceTex.GetPixel(i, j);
+
+                if (currentColorPixel == Color.white)
+                    continue;
+
+                Vector2Int currentPixel = new Vector2Int(i, j);
+                float magnitudePixel = (currentPixel - centerPixel).magnitude;
+
+                if (magnitudePixel < radius)
+                {
+                    float multiColor = -(magnitudePixel /radius) + 1.0f;
+                    Color newColor = color * (multiColor * powerInfluence);
+
+                    Color finalColor = (newColor + currentColorPixel) / 2.0f;
+                    finalColor.a = 1.0f;
+
+                    influenceTex.SetPixel(i, j, finalColor);
+                }
+            }
+        }
     }
 }
+
